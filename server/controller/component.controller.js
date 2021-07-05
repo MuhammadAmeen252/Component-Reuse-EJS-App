b82fb115bbbdaa04f840b63c9564215640d391da
MuhammadAmeen252 <sheikh.ameen252@gmail.com>
2021-07-05 21:11:47 +0500

    added new files

--- server/controller/component.controller.js
+++ server/controller/component.controller.js
@@ -3,12 +3,30 @@ const axios = require('axios')
 
 //EJS route render controllers
 const index = (req, res, next) => {
-    res.render('index')
+    axios.get('http://localhost:3000/api/getAllComponents')
+    .then(function(response){
+        //console.log(response.data)
+        res.render('index',{components : response.data})
+    })
+    .catch(e => {
+        res.send(e)
+    })
+};
+
+const updateComponentPage = (req, res, next) => {
+    //get comp whose id is matching with params Id
+    axios.get('http://localhost:3000/api/Component/'+req.query.id)
+    .then(function(compToUpdate){
+        //console.log(compToUpdate)
+        res.render('updateComponent',{component: compToUpdate.data})
+    })
 };
 
 const addComponentForm = (req, res, next) => {
     res.render('addComponentForm', {});
 };
+
+ 
 
 //API routes
 const addComponent = async(req,res) => {
@@ -38,7 +56,8 @@ const getComponent = async (req, res, next) => {
             return res.status(404).send('Nothing to Display')
         }
         const component = await Component.find({'name':compName})
-        //console.log('com', component)
+        console.log('com', component)
+        console.log('comName', compName)
         //res.send(component)
         if(component.length > 0){
             let compIndex = 0
@@ -69,9 +88,63 @@ const getComponent = async (req, res, next) => {
     }
 }
 
+const getAllComponents = async(req, res, next) => {
+    try{
+        const components = await Component.find({})
+        res.status(200).send(components)
+    }
+    catch(e){
+        res.status(404).send(e.message)
+    }
+}
+
+const updateComponent = async(req, res, next) => {
+    const updates = Object.keys(req.body)
+    try{
+        const id = req.params.id
+        const component = await Component.findById(id)
+        //updates all the fields that are given in body by looping
+        updates.forEach((update) => component[update] = req.body[update])
+        await component.save()
+        // res.redirect('/')
+        res.send('updated component successfully!')
+    }
+    catch(e){
+        res.status(404).send(e.message)
+    }
+}
+
+const deleteComponent = async(req, res, next) => {
+    try{
+        const id = req.params.id
+        const component = await Component.findById(id)
+        await component.remove()
+        res.status(200).send(component)
+    }
+    catch(e){
+        res.status(404).send(e.message)
+    }
+}
+
+const getComponentById = async(req, res, next) => {
+    try{
+        const id = req.params.id
+        const component = await Component.findById(id)
+        res.status(200).send(component)
+    }
+    catch(e){
+        res.status(404).send(e.message)
+    }
+}
+
 module.exports = {
     addComponent,
     getComponent,
     index,
-    addComponentForm
+    addComponentForm,
+    getAllComponents,
+    getComponentById,
+    updateComponent,
+    deleteComponent,
+    updateComponentPage
 }
--- server/routes/component.routes.js
+++ server/routes/component.routes.js
@@ -5,9 +5,14 @@ const componentController = require('../controller/component.controller');
 //renders
 route.get('/', componentController.index)
 route.get('/addComponent', componentController.addComponentForm)
+route.get('/updateComponent', componentController.updateComponentPage)
 
 //API routes
 route.post('/api/addComponent', componentController.addComponent)
 route.get('/api/getComponent/:name', componentController.getComponent)
+route.get('/api/getAllComponents', componentController.getAllComponents)
+route.get('/api/Component/:id', componentController.getComponentById)
+route.patch('/api/Component/:id', componentController.updateComponent)
+route.delete('/api/Component/:id', componentController.deleteComponent)
 
 module.exports = route
