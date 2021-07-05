cb2e2c2868f8a92d1c5561dda71a8bf5a2d82f74
MuhammadAmeen252 <sheikh.ameen252@gmail.com>
2021-07-05 21:11:47 +0500

    added new files

--- public/js/index.js
+++ public/js/index.js
@@ -11,3 +11,44 @@ $('#search-button').click(function() {
     }
     
 })
+
+//updating component after from submission
+$('#update_component').submit(function(event){
+
+    event.preventDefault()
+    //after submitting form all the submitted data will be inside this array
+    var unindexedArray = $(this).serializeArray()
+    let data = {'id':'','name':'','code':''}
+    let keys = Object.keys(data)
+    unindexedArray.map((item,i) => {
+        data[keys[i]] = item['value']
+    })
+
+    var request = {
+        "url":`http://localhost:3000/api/Component/${data.id}`,
+        "method":"PATCH",
+        "data":data
+    }
+    $.ajax(request).done(function(res) {
+        
+        alert('Component updated successfully!')
+        window.location.href = '/'
+    })
+})
+
+//deleting component
+$('table > tbody > tr > td > a.delete').click(function() {
+    var id =$('table > tbody > tr > td > a.delete').attr("data-id")
+    console.log('id',id)
+    var request = {
+        "url":`http://localhost:3000/api/Component/${id}`,
+        "method":"DELETE",
+    }
+    if(confirm("Are you sure you want to delete this Component?")){
+        $.ajax(request).done(function(res) {
+            alert('Component deleted successfully!')
+            location.reload()
+        })
+    }
+
+})
